6d6db848b28284c607f2a673eeb1e12c1d9d0052
LucasSResende <lucasresendesrs@gmail.com>
2024-11-09 11:14:45 -0500

    Initial Commit

---
+++ AppAula3/AppAula3App.swift
@@ -0,0 +1,17 @@
+//
+//  AppAula3App.swift
+//  AppAula3
+//
+//  Created by user269368 on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppAula3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppAula3Tests/AppAula3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AppAula3Tests.swift
+//  AppAula3Tests
+//
+//  Created by user269368 on 09/11/24.
+//
+
+import Testing
+@testable import AppAula3
+
+struct AppAula3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
